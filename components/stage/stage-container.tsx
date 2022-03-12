5c18735cab72e2032612ee9f80c7d32d414b68b6
pmakowski23 <praca.makowski@gmail.com>
2022-03-12 19:27:59 +0100

    CU-24atxw1 - Remove stage login validation

--- components/stage/stage-container.tsx
+++ components/stage/stage-container.tsx
@@ -1,10 +1,8 @@
 import useSWR from 'swr';
 import cn from 'classnames';
 import { Stage } from '@lib/types';
-import useLoginStatus from '@lib/hooks/use-login-status';
 import styles from './stage-container.module.css';
 import styleUtils from '../utils/utils.module.css';
-import ConfEntry from '@components/conference/conf-entry';
 import LiveChat from '@components/stage/live-chat';
 
 type Props = {
@@ -21,31 +19,26 @@ export default function StageContainer({ stage, allStages }: Props) {
   const updatedStages = response.data || [];
   const updatedStage = updatedStages.find((s: Stage) => s.slug === stage.slug) || stage;
 
-  const { loginStatus, mutate } = useLoginStatus();
   const streamId = updatedStage.stream.split('/')[4];
 
   return (
     <div className={styles.container}>
       <div className={styles.streamContainer}>
-        {loginStatus === 'loggedIn' ? (
-          <div className={cn(styles.stream, styleUtils.appear, styleUtils['appear-first'])}>
-            <iframe
-              allow="autoplay; picture-in-picture"
-              allowFullScreen
-              frameBorder="0"
-              src={`${updatedStage.stream}?autoplay=1&mute=1&theme=dark`}
-              title={updatedStage.name}
-              width="100%"
-            />
-            <div className={cn(styles.bottom, styleUtils.appear, styleUtils['appear-second'])}>
-              <div className={styles.messageContainer}>
-                <h2 className={styles.stageName}>{stage.name}</h2>
-              </div>
+        <div className={cn(styles.stream, styleUtils.appear, styleUtils['appear-first'])}>
+          <iframe
+            allow="autoplay; picture-in-picture"
+            allowFullScreen
+            frameBorder="0"
+            src={`${updatedStage.stream}?autoplay=1&mute=1&theme=dark`}
+            title={updatedStage.name}
+            width="100%"
+          />
+          <div className={cn(styles.bottom, styleUtils.appear, styleUtils['appear-second'])}>
+            <div className={styles.messageContainer}>
+              <h2 className={styles.stageName}>{stage.name}</h2>
             </div>
           </div>
-        ) : loginStatus === 'loading' ? null : (
-          <ConfEntry onRegister={() => mutate()} />
-        )}
+        </div>
       </div>
       <LiveChat liveId={streamId} />
     </div>
