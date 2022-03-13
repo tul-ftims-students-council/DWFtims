3b9f93401d8b60ca3acc9776804e1a3d03cc7f26
pmakowski23 <praca.makowski@gmail.com>
2022-03-13 12:27:16 +0100

    CU-24atxw1 - Revert changes

--- next.config.js
+++ next.config.js
@@ -2,10 +2,5 @@ module.exports = {
   images: {
     domains: ['a.storyblok.com'],
     imageSizes: [24, 64, 300]
-  },
-  reactStrictMode: true,
-  experimental: {
-    reactRoot: true,
-    runtime: 'nodejs' // but it also seems to crash when 'edge' is chosen
   }
 };
