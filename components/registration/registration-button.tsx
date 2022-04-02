a915209632c9cc23df3b5b26890b5cd7b25aed6e
Patryk Makowski <praca.makowski@gmail.com>
2022-04-02 16:07:13 +0200

    Add new button

--- components/registration/registration-button.tsx
+++ components/registration/registration-button.tsx
@@ -13,26 +13,42 @@ const RegistrationButton = ({
 }: RegistrationButtonProps) => {
   return (
     <div
-      className={cn(styles.wrapper, styleUtils.appear, styleUtils['appear-fifth'], {
-        [styles['wrapper-hidden']]: isRegistrationStarted,
-        [styleUtils.disappear]: isRegistrationStarted
-      })}
+      className={cn(
+        styles.wrapper,
+        styleUtils.appear,
+        styleUtils["appear-fifth"],
+        {
+          [styles["wrapper-hidden"]]: isRegistrationStarted,
+          [styleUtils.disappear]: isRegistrationStarted,
+        }
+      )}
     >
-      <button
-        type="submit"
-        className={cn(styles.register, styleUtils['center-el'], {
-          [styleUtils['move-to-left']]: isRegistrationStarted
-        })}
-        onClick={handleStartingRegistration}
-      >
-        Zapisz się na wydarzenia!
-      </button>
       <div
-        className={cn(styles['only-for'], styleUtils['center-el'], {
-          [styleUtils['move-to-left']]: isRegistrationStarted
+        className={cn(styles["only-for"], styleUtils["center-el"], {
+          [styleUtils["move-to-left"]]: isRegistrationStarted,
         })}
       >
-        Tylko dla <span className={styles.students}>Studentów</span>.
+        Zapisz się na <span className={styles.students}>wydarzenie</span>!
+      </div>
+      <div className={styles["buttons-wrapper"]}>
+        <button
+          type="submit"
+          className={cn(styles.register, styleUtils["center-el"], {
+            [styleUtils["move-to-left"]]: isRegistrationStarted,
+          })}
+          onClick={handleStartingRegistration}
+        >
+          Dla studentów
+        </button>
+        <a href="https://forms.samorzad.p.lodz.pl/zapisy-na-szkolenia-dzien-wydzialu-ftims/">
+          <button
+            className={cn(styles.register, styleUtils["center-el"], {
+              [styleUtils["move-to-left"]]: isRegistrationStarted,
+            })}
+          >
+            Dla uczniów szkół średnich
+          </button>
+        </a>
       </div>
     </div>
   );
